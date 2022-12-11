b9c6155ea6a66c9a69c58f05d7fe00bb911eba7c
paulGeoghegan <56843333+paulGeoghegan@users.noreply.github.com>
2022-12-11 17:40:42 +0000

    Working on adding website interaction

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const gv = require("@google-cloud/vision");
+const express = require("express");
 
 //Gets key from .env file
 const google_vision_key=process.env.gvkey;
@@ -7,10 +8,27 @@ const google_vision_key=process.env.gvkey;
 //Creates client
 const gv_client = new gv.ImageAnnotatorClient(google_vision_key);
 
-//Gets description for image
-gv_client.labelDetection("./test.jpg").then((results) => {
-	const labels = results[0].labelAnnotations;
+//Sets up express server
+const app = express(
 
-	console.log("Labels:");
-	labels.forEach((label) => console.log(label.description));
+);
+
+app.listen(3000, function() {
+	console.log('Server running on port 3000');
+});
+
+app.get("/", function(req, res) {
+	res.sendfile("./home.html");
+});
+
+app.post("/getAltText", function(req, res) {
+
+	//Gets description for image
+	gv_client.labelDetection("./test.jpg").then((results) => {
+		const labels = results[0].labelAnnotations;
+
+		console.log("Labels:");
+		labels.forEach((label) => console.log(label.description));
+	});
+
 });
