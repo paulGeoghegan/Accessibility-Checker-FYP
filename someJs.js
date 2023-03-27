b1fa57ad773a3457a52abd2258b46a31ce138875
paulGeoghegan <mrpaultdgeoghegan@gmail.com>
2023-03-27 23:47:01 +0000

    Updating the bad page example

---
+++ someJs.js
@@ -0,0 +1,8 @@
+
+//This will select my button
+document.getElementById("myButton").addEventListener("click",function() {
+	//Gets the text from the text box
+	let myText = document.getElementById("myTextbox").value;
+	//Prints the text
+	console.log(myText);
+});
